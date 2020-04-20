7110fcd821b68155cf1d612e119a9543322d8a46
alexgordontlv <alexaybn@gmail.com>
2020-04-20 21:06:14 +0000

    dd

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
 import csv
-
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+import chekmypassword
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,12 +15,6 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
-def write_to_file(data):
-  with open('database.txt', mode='a') as database:
-    email = data["email"]
-    subject = data["subject"]
-    message = data["message"]
-    file = database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
   with open('database.csv', newline='', mode='a') as database2:
@@ -33,9 +31,41 @@ def submit_form():
       try:
         data = request.form.to_dict()
         write_to_csv(data)
+        send_my_email(data)
         return redirect('/thankyou.html')
       except:
         return 'did not save to database'
     else:
       return 'something went wrong. Try again!'
+
+
+@app.route('/submit_form2', methods=['POST', 'GET'])
+def submit_form2():
+    if request.method == 'POST':
+  #    try:
+ #       data = request.form.to_dict()
+ #       password = chekmypassword.check(data["password"])
+        return redirect("https://github.com/alexgordontlv/passwordcheck")
+
+   #   except:
+ #       return 'did not save to database'
+  #  else:
+  #    return 'something went wrong. Try again!'
+
+def send_my_email(data):
+    emaildata = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    email = EmailMessage()
+    email['from'] = emaildata
+    email['to'] = "alexaybn@gmail.com"
+    email['subject'] = subject
+    email.set_content(message)
+
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('alexgordon.tlv@gmail.com', 'lopinking89')
+        smtp.send_message(email)
+
 
